3948e081b7c2953f164e8be88774f34ff8ec2b4a
iyedkh <111179698+Iyedkh@users.noreply.github.com>
2025-06-29 14:35:15 +0100

    deploy 2.2

--- backend/config/socket.js
+++ backend/config/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173","https://illustrious-maamoul-c1fa94.netlify.app/login"],
+    origin: ["http://localhost:5173","https://illustrious-maamoul-c1fa94.netlify.app"],
 
     credentials: true
   },
